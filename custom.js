940ee0356919643619d732d04869b72185f0651a
Glitch (taiko-web) <none>
2022-02-14 23:03:40 +0000

    🍥🛋 Checkpoint ./custom.js:38541371/19317

--- custom.js
+++ custom.js
@@ -1,8 +1,8 @@
 addEventListener("ready", () => {
 	class Plugin extends Patch{
 		name = "Custom patches"
-		version = "22.02.15"
-		description = "Does not include the custom code in loader.js"
+		version = "22.02.14"
+		description = "Opens the correct privacy file. Removes Random Song and an extra Back from the song select when no songs are loaded. Adds an Application Form button to the tutorial. Does not include the custom code in loader.js, which uses correct paths for api files."
 		author = "Katie Frogs"
 		
 		load(){
