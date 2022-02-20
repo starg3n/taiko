0672675936121e561bb3877c9d519128e3d65e20
Glitch (taiko-web) <none>
2022-02-20 03:56:54 +0000

    🎐🏝 Checkpoint ./loader-patch.js:38541371/1624

---
+++ loader-patch.js
@@ -0,0 +1,12 @@
+;(() => {
+	var initStr = Loader.prototype.init.toString()
+	initStr = initStr.slice(initStr.indexOf("{") + 1, initStr.lastIndexOf("}"))
+	initStr = initStr.replace('""/api/config"', '"/api/config.json"')
+	Loader.prototype.init = Function("callback", initStr)
+	
+	var runStr = Loader.prototype.run.toString()
+	runStr = runStr.slice(runStr.indexOf("{") + 1, runStr.lastIndexOf("}"))
+	runStr = runStr.replace('""/api/categories"', '"/api/categories.json"')
+	runStr = runStr.replace('""/api/songs"', '"/api/songs.json"')
+	Loader.prototype.run = Function(runStr)
+})()
