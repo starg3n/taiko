3f50712580c57694d58b572d21583f24eb315cf8
Glitch (taiko-web) <none>
2022-11-03 17:28:33 +0000

    👽🐡 Checkpoint ./api/config.json:38541371/6931 ./loader-patch.js:38541371/2250 ./src/js/loader.js:38541371/1130

--- loader-patch.js
+++ loader-patch.js
@@ -1,12 +1,12 @@
 ;(() => {
 	var initStr = Loader.prototype.init.toString()
 	initStr = initStr.slice(initStr.indexOf("{") + 1, initStr.lastIndexOf("}"))
-	initStr = initStr.replace('"/api/config"', '"/api/config.json"')
+	initStr = initStr.replace('"api/config"', '"api/config.json"')
 	Loader.prototype.init = Function("callback", initStr)
 	
 	var runStr = Loader.prototype.run.toString()
 	runStr = runStr.slice(runStr.indexOf("{") + 1, runStr.lastIndexOf("}"))
-	runStr = runStr.replace('"/api/categories"', '"/api/categories.json"')
-	runStr = runStr.replace('ajax("/api/songs"', 'ajax("/api/songs.json"')
+	runStr = runStr.replace('"api/categories"', '"api/categories.json"')
+	runStr = runStr.replace('ajax("api/songs"', 'ajax("api/songs.json"')
 	Loader.prototype.run = Function(runStr)
 })()
